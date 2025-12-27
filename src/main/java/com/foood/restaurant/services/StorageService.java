715b25b62d6971147a9e9ef3bd91fd9fe93a3455
eng-yousef-hesham <Eng_yousef_hesham@hotmail.com>
2025-12-27 02:16:13 +0200

    feat: photo upload service added

---
+++ src/main/java/com/foood/restaurant/services/StorageService.java
@@ -0,0 +1,14 @@
+package com.foood.restaurant.services;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface StorageService {
+
+    String store(MultipartFile file, String fileName);
+
+    Optional<Resource> loadAsResource(String fileName);
+
+}
